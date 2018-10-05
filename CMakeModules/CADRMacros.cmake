3df2d3178f902fcc256ea59c1f7e7252fe8ee434
PCJohn <peciva@fit.vut.cz>
2018-10-05 15:20:23 +0200

    Added VulkanHelloTriangle example: - new code including pipeline setup and shaders - added custom commands to convert GLSL shaders to spir-v - fixed IDE folders in MSVC

--- CMakeModules/CADRMacros.cmake
+++ CMakeModules/CADRMacros.cmake
@@ -121,3 +121,17 @@ macro(cadr_report_find_status)
       endif()
    endif()
 endmacro()
+
+
+# creates custom commands to convert GLSL shaders to spir-v
+# and creates depsList containing name of files that should be included among the source files
+macro(add_shaders nameList depsList)
+	foreach(name ${nameList})
+		add_custom_command(COMMENT "Converting ${name} to spir-v..."
+		                   MAIN_DEPENDENCY ${name}
+		                   OUTPUT ${name}.spv
+		                   COMMAND ${CMAKE_SOURCE_DIR}/3rdParty/bin/glslangValidator.exe -V -x ${CMAKE_CURRENT_SOURCE_DIR}/${name} -o ${name}.spv)
+		source_group("Shaders" FILES ${name} ${CMAKE_CURRENT_BINARY_DIR}/${name}.spv)
+		list(APPEND ${depsList} ${name} ${CMAKE_CURRENT_BINARY_DIR}/${name}.spv)
+	endforeach()
+endmacro()
