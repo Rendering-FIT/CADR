db64211f659becd88ca853325118d04799fe63ec
PCJohn <peciva@fit.vut.cz>
2019-08-07 14:12:57 +0200

    glTFreader: Parsing simple triangle model.

--- examples/glTFreader/main.cpp
+++ examples/glTFreader/main.cpp
@@ -1,11 +1,31 @@
+#include <CadR/BufferData.h>
+#include <CadR/CadR.h>
+#include <CadR/Mesh.h>
 #include <vulkan/vulkan.hpp>
 #include <nlohmann/json.hpp>
-#include <iostream>
+#include <experimental/filesystem>
 #include <fstream>
+#include <iostream>
+#include <memory>
 
 using namespace std;
+using namespace std::experimental;
 
 using json=nlohmann::json;
+
+typedef logic_error gltfError;
+
+
+template<typename T,typename Map,typename Key>
+T mapGetWithDefault(const Map& m,const Key& k,T d)
+{
+	auto it=m.find(k);
+	if(it!=m.end())
+		return *it;
+	else
+		return d;
+}
+
 
 
 int main(int argc,char** argv) {
@@ -17,16 +37,127 @@ int main(int argc,char** argv) {
 			cout<<"Please, specify glTF file to load."<<endl;
 			return 1;
 		}
-		string fileName(argv[1]);
-		cout<<"Opening file \""<<fileName<<"\"..."<<endl;
+		filesystem::path filePath(argv[1]);
 
 		// open file
-		ifstream f(fileName);
-		f.exceptions(ifstream::badbit);
+		ifstream f(filePath);
+		if(!f.is_open()) {
+			cout<<"Can not open file "<<filePath<<"."<<endl;
+			return 1;
+		}
+		f.exceptions(ifstream::badbit|ifstream::failbit);
+
+		// init CADR
+		CadR::init();
 
 		// parse json
+		cout<<"Processing file "<<filePath<<"..."<<endl;
 		json j3;
 		f>>j3;
+
+		// print glTF info
+		cout<<endl;
+		cout<<"glTF info:"<<endl;
+		cout<<"   Version:     "<<j3["asset"]["version"]<<endl;
+		cout<<"   MinVersion:  "<<j3["asset"]["minVersion"]<<endl;
+		cout<<"   Generator:   "<<j3["asset"]["generator"]<<endl;
+		cout<<"   Copyright:   "<<j3["asset"]["copyright"]<<endl;
+		cout<<endl;
+
+		// read object lists
+		auto scenes=j3["scenes"];
+		auto nodes=j3["nodes"];
+		auto meshes=j3["meshes"];
+		auto accessors=j3["accessors"];
+		auto buffers=j3["buffers"];
+		auto bufferViews=j3["bufferViews"];
+
+		// print stats
+		cout<<"Stats:"<<endl;
+		cout<<"   Scenes:  "<<scenes.size()<<endl;
+		cout<<"   Nodes:   "<<nodes.size()<<endl;
+		cout<<"   Meshes:  "<<meshes.size()<<endl;
+		cout<<endl;
+
+		// mesh
+		if(meshes.size()!=1)
+			throw gltfError("Only files with one mesh are supported for now.");
+		auto& mesh=meshes[0];
+
+		// primitive
+		auto primitives=mesh["primitives"];
+		if(primitives.size()!=1)
+			throw gltfError("Only meshes with one primitive are supported for now.");
+		auto primitive=primitives[0];
+
+		// primitive data
+		auto attributes=primitive["attributes"];
+		if(primitive.find("indices")!=primitive.end())
+			throw gltfError("Unsupported functionality: indices.");
+		auto mode=mapGetWithDefault(primitive,"mode",4);
+		if(mode!=4)
+			throw gltfError("Unsupported functionality: mode is not 4 (TRIANGLES).");
+
+		// accessor
+		if(attributes.find("POSITION")==attributes.end() || attributes.size()!=1)
+			throw gltfError("Unsupported attribute configuration.");
+		size_t positionAccessorIndex=attributes["POSITION"];
+		auto positionAccessor=accessors.at(positionAccessorIndex);
+		if(positionAccessor.find("bufferView")==positionAccessor.end())
+			throw gltfError("Unsupported functionality: Omitted bufferView.");
+		size_t bufferViewIndex=positionAccessor["bufferView"];
+		size_t accessorOffset=mapGetWithDefault(positionAccessor,"byteOffset",0);
+		int componentType=positionAccessor["componentType"];
+		if(componentType!=5126) // float
+			throw gltfError("Invalid component type. Must be float.");
+		bool normalized=mapGetWithDefault(positionAccessor,"normalized",false);
+		size_t count=positionAccessor["count"];
+		if(count==0)
+			throw gltfError("Attribute count in accessor is 0.");
+		string type=positionAccessor["type"];
+		if(type!="VEC3")
+			throw gltfError("Invalid attribute type. Must be VEC3.");
+		if(positionAccessor.find("sparse")!=positionAccessor.end())
+			throw gltfError("Unsupported functionality: Property sparse.");
+
+		// bufferView
+		auto bufferView=bufferViews.at(bufferViewIndex);
+		size_t bufferIndex=bufferView["buffer"];
+		size_t bufferViewOffset=mapGetWithDefault(bufferView,"byteOffset",0);
+		size_t bufferViewLength=bufferView["byteLength"];
+		size_t stride=mapGetWithDefault(bufferView,"byteStride",0);
+		if(stride!=0)
+			throw gltfError("Unsupported functionality: Stride not zero.");
+
+		// buffer
+		auto buffer=buffers.at(bufferIndex);
+		filesystem::path bufferUri=mapGetWithDefault<string>(buffer,"uri","");
+		size_t bufferLength=buffer["byteLength"];
+
+		// open buffer file
+		filesystem::path bufferPath=
+			bufferUri.is_absolute()
+				?bufferUri
+				:filePath.parent_path()/bufferUri;
+		cout<<"Opening buffer "<<bufferPath<<"..."<<endl;
+		ifstream b(bufferPath);
+		if(!b.is_open()) {
+			cout<<"Can not open file "<<bufferPath<<"."<<endl;
+			return 1;
+		}
+		b.exceptions(ifstream::badbit|ifstream::failbit);
+
+		// read buffer file
+		CadR::BufferData data(count*12);
+		b.seekg(bufferViewOffset+accessorOffset);
+		b.read(data.get<istream::char_type>(),data.size);
+		b.close();
+
+		auto m=make_unique<CadR::Mesh>(CadR::AttribConfig{12},count,count,0);
+		//m->uploadAttrib(0,move(data));
+		//auto a=make_unique<cadR::AttribStorage>();
+		//auto p=make_unique<CadR::PrimitiveSet>();
+
 
 	// catch exceptions
 	} catch(vk::Error &e) {
