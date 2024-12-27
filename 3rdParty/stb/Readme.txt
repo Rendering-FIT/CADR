f27d57bce2c378de91b3b68f717e387cc51257ab
PCJohn <peciva@fit.vut.cz>
2024-12-27 09:48:45 +0100

    Added stb_image to 3rdParty.

---
+++ 3rdParty/stb/Readme.txt
@@ -0,0 +1,2 @@
+Downloaded from https://github.com/nothings/stb on 2024-12-18.
+License: Public domain (or MIT - dual licensed)
---
+++ 3rdParty/stb/update-from-repo.bat
@@ -0,0 +1,4 @@
+rmdir /s /q repo
+git clone --depth 1 https://github.com/nothings/stb.git repo
+move repo\stb_image.h .
+rmdir /s /q repo
