cb05c2ff772097502bf29bd888f3c119b5e0c003
PCJohn <peciva@fit.vut.cz>
2025-01-08 09:12:15 +0100

    Forgotten ImageAllocationRecord::releaseHandles().

--- src/CadR/ImageAllocation.cpp
+++ src/CadR/ImageAllocation.cpp
@@ -8,6 +8,31 @@ ImageAllocationRecord ImageAllocationRecord::nullRecord{ 0, 0, nullptr, nullptr,
 
 
 
+void ImageAllocationRecord::releaseHandles(VulkanDevice& device) noexcept
+{
+	// no CopyRecord attached
+	if(!copyRecord) {
+		device.destroy(image);
+		image = nullptr;
+		return;
+	}
+
+	// image handle
+	if(copyRecord->copyOpCounter==0) {
+		device.destroy(image);
+		image = nullptr;
+	}
+	else
+		copyRecord->imageToDestroy = image;
+
+	// detach CopyRecord
+	// (it will be destroyed when copyOpCounter or referenceCounter is decremented
+	// and both become zero)
+	copyRecord->imageAllocationRecord = nullptr;
+	copyRecord = nullptr;
+}
+
+
 #if 0
 void ImageAllocation::upload(const void* ptr, size_t numBytes)
 {
